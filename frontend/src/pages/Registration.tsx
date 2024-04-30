e749b738759030a43a1aafc3b034fcf24701788b
gabs <gabriellenatashaa@gmail.com>
2024-04-30 10:20:52 +0800

    Npm I MUI

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -1,5 +1,59 @@
+import {
+  FormControl,
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import { Navigate } from "react-router-dom";
+
 function Registration(): JSX.Element {
-  return <div></div>;
+  return (
+    <>
+      <TextField fullWidth id="username" label="Username" variant="standard" />
+
+      <TextField
+        fullWidth
+        id="password"
+        label="Password"
+        type="password"
+        variant="standard"
+      />
+
+      <TextField
+        fullWidth
+        id="full-name"
+        label="Full Name"
+        variant="standard"
+      />
+
+      <TextField
+        fullWidth
+        id="date-of-birth"
+        label="Date of Birth"
+        variant="standard"
+      />
+
+      <TextField fullWidth id="contact" label="Contact" variant="standard" />
+
+      <TextField
+        fullWidth
+        multiline
+        maxRows={2}
+        id="address"
+        label="Address"
+        variant="standard"
+      />
+
+      <Select fullWidth label="Role">
+        <MenuItem value={"DOCTOR"}>Doctor</MenuItem>
+        <MenuItem value={"PATIENT"}>Patient</MenuItem>
+      </Select>
+
+      <Button>Return</Button>
+    </>
+  );
 }
 
 export default Registration;
