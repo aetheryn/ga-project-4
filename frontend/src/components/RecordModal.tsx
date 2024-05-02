172946320b028b7ec6651845f5797c38956132be
gabs <gabriellenatashaa@gmail.com>
2024-05-03 02:04:09 +0800

    Finalise record modal

--- frontend/src/components/RecordModal.tsx
+++ frontend/src/components/RecordModal.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 import { Record } from "../classes/record";
 import { User } from "../classes/user";
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Button, TextField } from "@mui/material";
 import useFetch from "../hooks/useFetch";
 import UserContext from "../context/user";
@@ -17,21 +17,34 @@ interface ModalProps {
 function Overlay(props: ModalProps): JSX.Element {
   const { record, selectedUser, setShowRecord, getAllRecords } = props;
   const [isUpdating, setIsUpdating] = useState<boolean>(false);
+  const [doctor, setDoctor] = useState<User>({
+    id: 0,
+    username: "",
+    hash: "",
+    full_name: "",
+    date_of_birth: new Date(),
+    contact: 0,
+    address: "",
+    role: "",
+    pending_approval: false,
+  });
 
-  const subjRef = useRef<HTMLInputElement>(
+  const subjRef = useRef<HTMLTextAreaElement>(
     document.querySelector("#subjective")
   );
-  const objRef = useRef<HTMLInputElement>(document.querySelector("#objective"));
-  const assRef = useRef<HTMLInputElement>(
+  const objRef = useRef<HTMLTextAreaElement>(
+    document.querySelector("#objective")
+  );
+  const assRef = useRef<HTMLTextAreaElement>(
     document.querySelector("#assessment")
   );
-  const planRef = useRef<HTMLInputElement>(document.querySelector("#plan"));
+  const planRef = useRef<HTMLTextAreaElement>(document.querySelector("#plan"));
 
   const fetchData = useFetch();
 
   const userCtx = useContext(UserContext);
 
-  async function updateDetails() {
+  async function updateDetails(): Promise<void> {
     try {
       const response: any = await fetchData(
         "/details/" + record.id,
@@ -47,100 +60,140 @@ function Overlay(props: ModalProps): JSX.Element {
       );
       if (response.ok) {
         getAllRecords();
+        setIsUpdating(false);
       }
     } catch (error: any) {
       console.error(error.message);
     }
     console.log("Detail updated.");
-    setIsUpdating(false);
   }
+
+  async function getDoctorDetails() {
+    try {
+      const response: any = await fetchData(
+        "/users/" + record.doctor_id,
+        "POST",
+        undefined,
+        undefined
+      );
+
+      if (response.ok) {
+        setDoctor(response.data);
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getDoctorDetails();
+  }, []);
 
   return (
     <div className={styles.backdrop}>
       <div className={styles.modal}>
-        <div>
-          <h1>{selectedUser.full_name}</h1>
-        </div>
-        {!isUpdating && (
-          <>
-            <div>
-              <p>{record.subjective}</p>
-              <p>{record.objective}</p>
-              <p>{record.assessment}</p>
-              <p>{record.plan}</p>
-            </div>
+        <div className={styles.container}>
+          <h6 className={styles.name}>Record for {selectedUser.full_name}</h6>
+          <div className={styles.details}>
+            <label>Subjective</label>
+            {!isUpdating && <div>{record.subjective}</div>}
+            {isUpdating && (
+              <textarea
+                required
+                id="subjective"
+                ref={subjRef}
+                className={styles.forminput}
+                rows={4}
+                style={{ resize: "none" }}
+                defaultValue={record.subjective}
+              ></textarea>
+            )}
 
-            <Button className="col-md-6" onClick={() => setIsUpdating(true)}>
+            <label>Objective</label>
+            {!isUpdating && <div>{record.objective}</div>}
+            {isUpdating && (
+              <textarea
+                required
+                id="objective"
+                ref={objRef}
+                className={styles.forminput}
+                rows={4}
+                style={{ resize: "none" }}
+                defaultValue={record.objective}
+              ></textarea>
+            )}
+
+            <label>Assessment</label>
+            {!isUpdating && <div>{record.assessment}</div>}
+            {isUpdating && (
+              <textarea
+                required
+                id="assessment"
+                ref={assRef}
+                className={styles.forminput}
+                rows={4}
+                style={{ resize: "none" }}
+                defaultValue={record.assessment}
+              ></textarea>
+            )}
+
+            <label>Plan</label>
+            {!isUpdating && <div>{record.plan}</div>}
+            {isUpdating && (
+              <textarea
+                required
+                id="plan"
+                ref={planRef}
+                className={styles.forminput}
+                rows={4}
+                style={{ resize: "none" }}
+                defaultValue={record.plan}
+              ></textarea>
+            )}
+
+            {!isUpdating && (
+              <>
+                <label>Consultation</label>
+                <div>
+                  {record.created_at.toString().slice(0, 10)} by{" "}
+                  {doctor.full_name}
+                </div>
+              </>
+            )}
+          </div>
+
+          {!isUpdating && userCtx.loggedInUser.id === record.doctor_id && (
+            <button
+              className={styles.button}
+              onClick={() => setIsUpdating(true)}
+            >
               Update
-            </Button>
+            </button>
+          )}
 
-            <Button
-              className="return"
+          {!isUpdating && (
+            <button
+              className={`${styles.button} ${styles.return}`}
               onClick={() => setShowRecord(false)}
-              style={{
-                backgroundColor: "transparent",
-                borderColor: "transparent",
-              }}
             >
               &#x2190;
-            </Button>
-          </>
-        )}
+            </button>
+          )}
 
-        {isUpdating && (
-          <form>
-            <TextField
-              fullWidth
-              id="subjective"
-              label="Subjective"
-              multiline
-              maxRows={8}
-              variant="outlined"
-              inputRef={subjRef}
-              defaultValue={record.subjective}
-            ></TextField>
-
-            <TextField
-              fullWidth
-              id="objective"
-              label="Objective"
-              multiline
-              maxRows={8}
-              variant="outlined"
-              inputRef={objRef}
-              defaultValue={record.objective}
-            ></TextField>
-
-            <TextField
-              fullWidth
-              id="assessment"
-              label="Assessment"
-              multiline
-              maxRows={8}
-              variant="outlined"
-              inputRef={assRef}
-              defaultValue={record.assessment}
-            ></TextField>
-
-            <TextField
-              fullWidth
-              id="plan"
-              label="Plan"
-              multiline
-              maxRows={8}
-              variant="outlined"
-              inputRef={planRef}
-              defaultValue={record.plan}
-            ></TextField>
-
-            <Button onClick={() => updateDetails()} className="col-md-3">
-              Save
-            </Button>
-            <Button className="col-md-3" onClick={() => setIsUpdating(false)}>
-              Cancel
-            </Button>
-          </form>
-        )}
+          {isUpdating && (
+            <>
+              <button onClick={() => updateDetails()} className={styles.button}>
+                Save
+              </button>
+              <button
+                className={`${styles.button} ${styles.return}`}
+                onClick={() => setIsUpdating(false)}
+              >
+                Cancel
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
